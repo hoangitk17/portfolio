89a4024e42d31bc242a6597e4a84d370b0fe2c9c
Nguyen Van Hoang <hoangitk17@gmail.com>
2022-03-20 15:43:11 +0700

    Add contact section

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,39 +1,40 @@
 $(document).ready(function () {
-  $(window).scroll(function() {
+  $(window).scroll(function () {
     if (this.scrollY > 20) {
-      $('.navbar').addClass('sticky');
+      $(".navbar").addClass("sticky");
     } else {
-      $('.navbar').removeClass('sticky');
+      $(".navbar").removeClass("sticky");
     }
 
     if (this.scrollY > 1) {
-      $('.scroll-up-btn').addClass("show"); 
+      $(".scroll-up-btn").addClass("show");
     } else {
-      $('.scroll-up-btn').removeClass("show"); 
+      $(".scroll-up-btn").removeClass("show");
     }
-  })
+  });
 
   var typed = new Typed(".typing", {
-    strings: ["Youtuber", "Developer", "Designer", "Freelancer"],
+    strings: ["Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
-  })
+  });
+
   var typed2 = new Typed(".typing-2", {
-    strings: ["Youtuber", "Developer", "Designer", "Freelancer"],
+    strings: ["Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
-  })
+  });
 
-  $('.scroll-up-btn').click(function () {
-    $('html').animate({ scrollTop: 0 });
-  })
+  $(".scroll-up-btn").click(function () {
+    $("html").animate({ scrollTop: 0 });
+  });
 
-  $('.menu-btn').click(function () {
-    $('.navbar .menu').toggleClass("active");
-    $('.navbar i').toggleClass("active");
-  })
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".navbar i").toggleClass("active");
+  });
 
   var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
@@ -58,4 +59,6 @@ $(document).ready(function () {
       slideShadows: true,
     },
   });
-})
+
+  AOS.init();
+});
